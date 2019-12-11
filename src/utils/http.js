16f7b9f5bf5f0beefd9460d3ef55645c35d836e6
twistedFate <851004971@qq.com>
2019-12-11 10:33:08 +0800

    封装axios请求
    封装axios请求

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,125 +1,104 @@
-/**
- * http配置
- */
-// 引入axios
 import axios from 'axios'
-// import store from './../store'
 import router from '@/router'
-import Vue from 'vue'
+import baseUrl from '@/api/baseUrl.js'
 
-// 超时时间
+// 跨域
+axios.defaults.withCredentials = true
+//设置不同环境的baseUrl
+axios.defaults.baseURL = baseUrl.apiBaseUrl
 axios.defaults.timeout = 100000
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-// http请求拦截器
-axios.interceptors.request.use(
-  config => {
-    //TODO 请求拦截
-    console.log("config:", config);
-    // Vue.$vux.loading.show({
-    //   text: '加载中...'
-    // });
-
-    return config
-  }, error => {
-    console.log("http请求进入拦截器,异常");
-    // Vue.$vux.loading.hide();
-    return Promise.reject(error)
-  })
-
-// http响应拦截器
-axios.interceptors.response.use(data => {//
-  console.log("http响应拦截器", data);
-//   Vue.$vux.loading.hide();
-  return data
+// request拦截器
+axios.interceptors.request.use(config => {
+  return config
 }, error => {
-  if (error.response) {
-    // Vue.$vux.loading.hide();
-    //TODO 待处理
-    switch (error.response.status) {
-      case 401:
-        /**token未授权或token授权失败，过期等等**/
-        // 401 清除token信息并跳转到登录页面
-        // store.commit(types.LOGOUT);
-        route.replace({
-          path: '/',
-          // query: {redirect: routers.currentRoute.fullPath}
-        });
-        break;
-      case 404:
-        /**未找到页面**/
-        // 404 跳转到404页面
-        // Vue.$vux.toast.show({
-        //   text: '请求异常',
-        //   type: 'warn'
-        // });
-        break;
-      case 403:
-        /**未找到页面**/
-        // 403 跳转到登录页面
-        // store.commit('SIGNOUT');
-        router.push({
-        path: '/',
-        // query: {redirect: router.currentRoute.fullPath}
-      });
-        break;
-      case 504:
-        // Vue.$vux.toast.show({
-        //   text: '网络异常',
-        //   type: 'warn'
-        // });
-        break;
-    }
-  }
-//   Vue.$vux.loading.hide();
-  return Promise.reject(error)
+  Promise.reject(error)
 })
 
-let base = '/api'
+// 控制 返回 401 时的弹出框次数
+let isMultiEvent = false
 
-export const postRequest = (url, params) => {
-  // 在请求头添加票证
-//   let _Authorization = store.getters.getUserInfo.Authorization;
-    let _Authorization = ''
-  return axios({
-    method: 'post',
-    url: `${base}${url}`,
-    data: params,
-    headers: {"Authorization": _Authorization}
-  });
-}
+// respone拦截器
+axios.interceptors.response.use(
+  response => {
+    /**
+     * code为非0时，返回错误信息 与后端约定code值
+     */
+    const res = response.data
+    if (res.resCode && res.resCode !== 0) {
+      return Promise.reject(res)
+    } else {
+      return response.data
+    }
+  },
+  error => {
+    // 请求无返回 超时/网络错误
+    if (!error.response) {
+      if (!isMultiEvent) {
+        alert("请求失败，请检查网络或服务器状态")
+        isMultiEvent = true
+        setTimeout(() => {
+          isMultiEvent = false
+        }, 3000)
+      }
+      // router.replace({
+      //   name: 'home',
+      //   query: { redirect: router.currentRoute.name }
+      // })
+    } else {
+      switch (error.response.status) {
+        case 401:
+          console.log('401')
+          if (!isMultiEvent) {
+            alert("登录超时，请重新登录")
+            /**
+             * 处理登录返回的情况
+             */
+            // 普通401拦截直接返回到登录页面
+            router.push({ name: 'home' })
+            isMultiEvent = true
+            setTimeout(() => {
+              isMultiEvent = false
+            }, 3000)
+          }
+          break
+        case 500:
+          console.log('500')
+          break
+        default:
+          console.log("输出错误信息")
+          break
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 export const getRequest = (url, params) => {
-  // 在请求头添加票证
-  // let _Authorization = store.getters.getUserInfo.Authorization;
-  let _Authorization = ''
   return axios({
     method: 'get',
-    url: `${base}${url}`,
-    data: params,
-    headers: {"Authorization": _Authorization}
+    url: `${url}`,
+    data: params
   });
 }
-
+export const postRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${url}`,
+    data: params
+  });
+}
 export const putRequest = (url, params) => {
-  // 在请求头添加票证
-  // let _Authorization = store.getters.getUserInfo.Authorization;
   return axios({
     method: 'put',
-    url: `${base}${url}`,
-    data: params,
-    headers: {"Authorization": _Authorization}
+    url: `${url}`,
+    data: params
   });
 }
-
 export const deleteRequest = (url, params) => {
-  // 在请求头添加票证
-  // let _Authorization = store.getters.getUserInfo.Authorization;
-  let _Authorization = ''
   return axios({
     method: 'delete',
-    url: `${base}${url}`,
-    data: params,
-    headers: {"Authorization": _Authorization}
+    url: `${url}`,
+    data: params
   });
 }
